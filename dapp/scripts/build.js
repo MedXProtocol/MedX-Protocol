2c59540a3ca7ca4a91bd36da9b54af105ec99206
Brendan Asselstine <mail.asselstine@gmail.com>
2018-09-21 14:32:40 -0700

    Fixed dapp build

--- dapp/scripts/build.js
+++ dapp/scripts/build.js
@@ -1,8 +1,8 @@
 'use strict';
 
 // Do this as the first thing so that any code reading it knows the right env.
-process.env.BABEL_ENV = 'production';
-process.env.NODE_ENV = 'production';
+process.env.BABEL_ENV = process.env.BABEL_ENV || 'production'
+process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
 // Makes the script crash on unhandled rejections instead of silently
 // ignoring them. In the future, promise rejections that are not handled will
@@ -25,9 +25,13 @@ var filesize = require('filesize');
 var gzipSize = require('gzip-size').sync;
 var webpack = require('webpack');
 
-var config = require('../config/webpack.config.dev');
+var config
 if (process.env.NODE_ENV === 'production') {
+  console.log('Using production config')
   config = require('../config/webpack.config.prod');
+} else {
+  console.log('Using development config')
+  config = require('../config/webpack.config.dev');
 }
 
 var paths = require('../config/paths');
