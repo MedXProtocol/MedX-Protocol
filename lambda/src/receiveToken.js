3b81bd1f1490487aabb7d356985716732de083d5
Brendan Asselstine <mail.asselstine@gmail.com>
2018-09-21 15:38:21 -0700

    Updated civic app keys

--- lambda/src/receiveToken.js
+++ lambda/src/receiveToken.js
@@ -1,11 +1,11 @@
 const civicSip = require('civic-sip-api');
-const _appId = process.env.appId;
-const _privateKey = process.env.pvtKey;
-const _secret = process.env.secret;
+const _appId = process.env.REACT_APP_CIVIC_APP_ID;
+const _privateKey = process.env.REACT_APP_CIVIC_PRIVATE_KEY;
+const _secret = process.env.REACT_APP_CIVIC_SECRET;
 
 exports.handler = (event, context, callback) => {
 	const body = JSON.parse(event.body);
-	
+
 	console.log("Received Token [" + body.jwtToken + "]");
 	const civicClient = civicSip.newClient({
 	  appId: _appId,
@@ -14,7 +14,7 @@ exports.handler = (event, context, callback) => {
 	});
 
 	civicClient.exchangeCode(body.jwtToken)
-		.then((userData) => {	        
+		.then((userData) => {
 	        console.log('userData = ', JSON.stringify(userData, null, 4));
             const successResponse = {
 		        "statusCode": 200,
@@ -28,5 +28,5 @@ exports.handler = (event, context, callback) => {
 	    .catch((error) => {
 	        console.log(error);
 	        callback(error);
-	    });	
+	    });
 };
