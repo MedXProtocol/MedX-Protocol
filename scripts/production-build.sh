aded363d63ed729098b6b75d3c0d5160b4c18aff
Brendan Asselstine <mail.asselstine@gmail.com>
2018-09-24 14:28:55 -0700

    Merged production and staging build scripts into single script

--- scripts/production-build.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/sh
-
-truffle compile && \
-npm run migrate -- --network ropsten && \
-cd lambda && \
-./lambda-build.sh && \
-cd .. && \
-cd dapp && npm i && node --max-old-space-size=4096 scripts/build.js && cd ..
--- scripts/staging-build.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/sh
-npm run install && \
-npm run compile && \
-cd lambda && \
-./lambda-build.sh && \
-cd .. && \
-cd dapp && npm i && node --max-old-space-size=4096 scripts/build.js  && cd ..
